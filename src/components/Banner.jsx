d312f66c37be7d92718b1c9c28b10a6a73a92bc6
Arman shekh <mdarmanshekh4@gmail.com>
2025-01-24 23:05:20 +0530

    changing temp

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import arman from '../assets/arman.png'
 import { Reveal } from './Reveal'
+import arman1 from '../assets/IMG_20250124_215249.png'
 
 function Banner() {
     return (
@@ -8,23 +9,23 @@ function Banner() {
             <div className='bannrMain'>
                 <div className='bannerContainer'>
                     <div className='bannerDiv'>
-                        
+
                         <Reveal>
-                            <div className='myname'>Hy! I  Am <br /><span style={{ color: "#5f5d5c", fontSize:"35px" }}>Md Arman Shekh</span></div>
-                       
-                            <p style={{ color: "#5f5d5c", letterSpacing:"1px",  fontFamily: "sans-serif" }}><span style={{ color: "#5f5d5c" }}>Hi</span>, I m Arman Shekh having 2.5-year experienced React developer dedicated to crafting seamless and user-friendly web experiences. Seeking collaborative opportunities to excel in front-end development.</p>
+                            <div className='myname'>Hy! I  Am <br /><span style={{ color: "#5f5d5c", fontSize: "35px" }}>Md Arman Shekh</span></div>
+
+                            <p style={{ color: "#5f5d5c", letterSpacing: "1px", fontFamily: "sans-serif" }}><span style={{ color: "#5f5d5c" }}>Hi</span>, I m Arman Shekh having 2.5-year experienced React developer dedicated to crafting seamless and user-friendly web experiences. Seeking collaborative opportunities to excel in front-end development.</p>
 
                         </Reveal>
                         <Reveal>
-                        <div style={{ display: "flex", gap: "20px" }}>
-                            <div><a className='button-81' href="tel:7091554628">Let's Talk &nbsp;  <i class="fa fa-phone"></i></a></div>
-                            <div><a className='button-81' href="/src/assets/frontenddeveloperresume.pdf">Download CV &nbsp; <i class="fa fa-download"></i></a></div>
-                        </div>
+                            <div style={{ display: "flex", gap: "20px" }}>
+                                <div><a className='button-81' href="tel:7091554628">Let's Talk &nbsp;  <i class="fa fa-phone"></i></a></div>
+                                <div><a className='button-81' href="/src/assets/frontenddeveloperresume.pdf">Download CV &nbsp; <i class="fa fa-download"></i></a></div>
+                            </div>
                         </Reveal>
                         <div className='followme'>Follow Me  <i class="fa fa-long-arrow-right"></i> <span> &nbsp; &nbsp;
 
                             <a href="https://www.facebook.com/mdarman.shekh.5201"><i class="fa fa-facebook"></i></a> &nbsp;
-                             <a href="https://github.com/ArmanA12"><i class="fa fa-github-alt"></i></a> &nbsp;
+                            <a href="https://github.com/ArmanA12"><i class="fa fa-github-alt"></i></a> &nbsp;
                             <a href="https://www.instagram.com/arman_aishakar/"><i class="fa fa-instagram"></i></a>
 
                             &nbsp;   <a href="https://www.linkedin.com/in/arman-shekh12/"><i class="fa fa-linkedin"></i></a>
@@ -32,11 +33,30 @@ function Banner() {
                         </span></div>
                     </div>
                     <div style={{ display: "flex", justifyContent: "center" }} className='bannerDiv'>
-                        <div className='banner-div-bg'>
-                           <Reveal>
-                           <img style={{ zIndex: "100" , objectFit:'fill'}} src={arman} alt="img" />
-                           </Reveal>
-                            <div className='something'></div>
+                        <div className='banner-div-bg' style={{
+                            backgroundImage: `radial-gradient(circle, rgba(0,0,0,0.3) 1px, transparent 1px)`,
+                            backgroundSize: '20px 20px', // Size of the dots and spacing
+                            backgroundColor: 'rgba(255,255,255,0.02)',
+
+                        }}>
+                            <Reveal>
+                                <div style={{ display: "flex", justifyContent: "center", width:"340px", height:"340px", 
+                                    border:"1px solid #f2f2f2",
+                                    borderRadius:"100%",
+                                    background:"rgba(0,0,0,0.01)",
+                                    padding:"20px",
+                        
+                                 }} >
+                                <img style={{ zIndex: "100", objectFit: 'fill' }} src={arman1} alt="img" />
+                                </div>
+                            </Reveal>
+                            <div className='something'>
+                              +
+                            </div>
+                            <div className='something1'>
+                              +
+                            </div>
+
                         </div>
                     </div>
                 </div>
